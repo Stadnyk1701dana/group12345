90287a510d5d4acd0f5433ae3a6b82a1481ee5a9
Yulianna Stadnyk <ystadnyk@edunav.com>
2023-05-01 00:18:31 +0300

    correction theme

---
+++ correction.py
@@ -0,0 +1,8 @@
+
+smile_footprint = '\N{Footprints}'
+
+User_name =input ('Please, enter your name >>> ')
+User_age =input ('Please, enter your age >>> ')
+result =  'My name is ' + User_name + ", I am " + User_age + ' years old' + smile_footprint
+
+print (result)
