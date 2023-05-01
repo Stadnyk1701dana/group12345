f1ab5e5905b57de3a1d79f1398adfdbf23ceb9dc
Yulianna Stadnyk <ystadnyk@edunav.com>
2023-05-01 13:13:03 +0300

    slices of strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+#          0123456789
+my_love = 'I love python'
+new_love = my_love[2:5]
+print(new_love)
+
+new_love = my_love[-6:-1]
+new_love = my_love[-6:]
+new_love = my_love[:-5]
+new_love = my_love[::-1]
+print(new_love)
