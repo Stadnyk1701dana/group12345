f2daa088ce6e76df57d419c804d7a185870f9e5a
Yulianna Stadnyk <ystadnyk@edunav.com>
2023-05-01 23:11:25 +0300

    add and push ball/

--- ball.py
+++ ball.py
@@ -1,25 +1,5 @@
-r=int(input("Введіть радіус "))
-
-print("Коди умов до задачі: 1 -довжина кола, 2 - площа круга, 3 - обєм кулі" )
-
-cod=int(input("Введіть код до задачі, яку слід виконати "))
-
+import math
+r=int(input("Ввеіус кулі"))
 print("При r=",r)
-
-if cod==1:
-
-      l=2*math.pi*r
-
-      print("Довжина кола - ",round(l,2))
-
-elif cod==2:
-
-      s=math.pi*r*r
-
-      print("Площа круга - ",round(s,2))
-
-elif cod==3:
-
-      v=4/3*math.pi*math.pow(r,3)
-
-      print("Обєм кулі - ",round(v,2))
+V =4/3*math.pi*math.pow(r,3)
+print("Обєм кулі - ",round(V,2))
