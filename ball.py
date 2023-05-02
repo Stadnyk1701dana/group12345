09a16d687022c663389c7992f175975e1dd6d202
Yulianna Stadnyk <ystadnyk@edunav.com>
2023-05-02 09:09:32 +0300

    ball theme

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 import math
-r=int(input("Ввеіус кулі"))
-print("При r=",r)
-V =4/3*math.pi*math.pow(r,3)
-print("Обєм кулі - ",round(V,2))
+
+r = int(input("Ввеіус кулі"))
+print("При r = ", r)
+v = 4 / 3 * math.pi * math.pow(r, 3)
+print("Обєм кулі - ", round(v, 2))
